c3884b6e0fd01af0c263114335b25154265ec043
hywu1996 <harry.y.wu@gmail.com>
2019-01-03 00:18:49 -0500

    http module for registration

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const config = require('./config/database');
 const path = require('path');
 const authentication = require('./routes/authentication')(router);
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
+//development
+app.use(cors({
+    origin: 'http://localhost:4200'
+}));
+//--!development--
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.uri, (err) => {
