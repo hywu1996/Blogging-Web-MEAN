67d75fc309f12a67a7653e9cfe4312817eb263d6
hywu1996 <harry.y.wu@gmail.com>
2019-01-04 00:36:31 -0500

    Login Authentication, Headers, & JWT

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,11 +4,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes =  [
   { path: '', component: HomeComponent},         
   { path: 'dashboard', component: DashboardComponent },
   { path: 'register', component: RegisterComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'profile', component: ProfileComponent },
   { path: '**', component: HomeComponent}
 ];
 
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,8 +10,15 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RegisterComponent } from './register/register.component';
-
 import { AuthService } from './services/auth.service';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FlashMessagesModule } from 'angular2-flash-messages';
+import { JwtModule } from '@auth0/angular-jwt';
+
+export function tokenGetter() {
+  return localStorage.getItem('access_token');
+}
 
 @NgModule({
   declarations: [
@@ -19,13 +26,23 @@ import { AuthService } from './services/auth.service';
     NavbarComponent,
     HomeComponent,
     DashboardComponent,
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlashMessagesModule.forRoot(),
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        whitelistedDomains: ['example.com'],
+        blacklistedRoutes: ['example.com/examplebadroute/']
+      }
+    })
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
