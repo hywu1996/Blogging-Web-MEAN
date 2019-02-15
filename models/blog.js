f53cfdee1258cb02f43a314c37f4ceec20658fa8
Harry Wu <harry.y.wu@gmail.com>
2019-02-15 17:19:13 -0500

    blog incomplete

--- models/blog.js
+++ models/blog.js
@@ -80,12 +80,21 @@ const passwordValidators = [
     }
 ];
 
-const userSchema   = new Schema({
-    email: { type: String, required: true, unique: true, lowercase: true, validate: emailValidators },
-    username:  { type: String, required: true, unique: true, lowercase: true, validate: usernameValidators },
-    password: { type: String, required: true, validate: passwordValidators }
+const blogSchema = newSchema({
+    title: { type: String, required: true },
+    body: { type: String, required: true },
+    createdBy: { type: String },
+    createdAt: { type: Date, default: Date.now() },
+    likes: { type: Number, default: 0},
+    likedBy: {type: Array },
+    dislikes: { type: Number, default: 0},
+    dislikedBy: {type: Array },
+    comments: [
+        {
+            comment: { type: String },
+            commentator: { type: String }
+        }
+    ]
 });
-
-
 
 module.exports = mongoose.model('Blog', blogSchema);
