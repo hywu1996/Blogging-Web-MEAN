c7b505621750e09f8256bff2f54cca576b519d61
hywu1996 <harry.y.wu@gmail.com>
2019-02-20 18:42:29 -0500

    newBlog server working

--- routes/authentication.js
+++ routes/authentication.js
@@ -112,23 +112,23 @@ module.exports = (router) => {
             });
         }
     });
-    
+    // MIDDLEWARE
     //any routes require authorization comes after this
-    router.use( (req, res, next) => {
-        const token = req.headers['authorization'];
-        if (!token) {
-            res.json({ success: false, message: 'No token provided' });
-        } else {
-            jwt.verify(token, config.secret, (err,decoded) => {
-                if (err) {
-                    res.json({ success: false, message: 'Token invalid: ' + err });
-                } else {
-                    req.decoded = decoded;
-                    next();
-                }
-            });
-        }
-    });
+    // router.use( (req, res, next) => {
+    //     const token = req.headers['authorization'];
+    //     if (!token) {
+    //         res.json({ success: false, message: 'No token provided' });
+    //     } else {
+    //         jwt.verify(token, config.secret, (err,decoded) => {
+    //             if (err) {
+    //                 res.json({ success: false, message: 'Token invalid: ' + err });
+    //             } else {
+    //                 req.decoded = decoded;
+    //                 next();
+    //             }
+    //         });
+    //     }
+    // });
 
 
     router.get('/profile', (req,res) => {
